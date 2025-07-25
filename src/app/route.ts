b77ee13284930f38e46f8623884a9e57822e259f
Alan Reis <alanreisanjo@gmail.com>
2025-07-25 02:42:34 -0300

    Eu to sem tankar essa stack: Next com EJS

---
+++ src/app/route.ts
@@ -0,0 +1,16 @@
+import path from "node:path";
+import { renderFile } from "ejs";
+
+export async function GET() {
+	const pageFilePath = path.join(process.env.PWD || "", "src/views/index.ejs");
+
+	const pageContent = await renderFile(pageFilePath, {
+		questions: [],
+	});
+
+	return new Response(pageContent, {
+    headers: {
+      "Content-Type": "text/html; charset=UTF-8"
+    }
+  });
+}
